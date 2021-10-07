2a50a4aef9d6a145f58a795b0033c471d0b4de8d
boden <bodenharlowe21@gmail.com>
2021-10-07 10:08:08 -0700

    Added score and next question

--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -15,40 +15,68 @@ class MainActivity : AppCompatActivity() {
     lateinit var option3: Button
     lateinit var option4: Button
     lateinit var questionTextView: TextView
-    var Score : Int =0
-    var question: Int = 0
+    lateinit var quiz: Quiz
     lateinit var questions: List<Question>
+    lateinit var currQuestion : Question
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        wireWidgets()
+        setQuestions()
+        nextQuestion()
+
+        option1.setOnClickListener {
+            quiz.Score(0)
+            // check if there are more questions and if so, set up the next question.
+            nextQuestion()
+        }
+        option2.setOnClickListener{quiz.Score(1)
+            nextQuestion()
+        }
+        option3.setOnClickListener {quiz.Score(2)
+            nextQuestion()
+        }
+        option4.setOnClickListener {quiz.Score(3)
+            nextQuestion()
+        }
     }
-     fun wireWidgets(){
+
+    private fun setUpQuestionTextViews() {
+        questionTextView.text = currQuestion.question
+        option1.text = currQuestion.a1
+        option2.text = currQuestion.a2
+        option3.text = currQuestion.a3
+        option4.text = currQuestion.a4
+    }
+
+    fun wireWidgets() {
         option1 = findViewById(R.id.option_1)
-        option2 = findViewById(R.id.option_2 )
+        option2 = findViewById(R.id.option_2)
         option3 = findViewById(R.id.option_3)
         option4 = findViewById(R.id.option_4)
         questionTextView = findViewById(R.id.Question)
-     }
-    fun nextQuestion(){
-    var q: Question = questions[question]
-        questionTextView.text = q.text
-        option1.text = q.a1
-        option2.text = q.a2
-        option3.text = q.a3
-        option4.text = q.a4
-
     }
+
+    fun nextQuestion() {
+        if(quiz.hasmorequestions()) {
+            currQuestion = quiz.getNextQuestion()
+            setUpQuestionTextViews()
+        } else {
+            // if there aren't more questions, display final score, hide the other UI
+        }
+    }
+
+
     fun setQuestions() {
         val inputStream = resources.openRawResource(R.raw.questions)
         val jsonText = inputStream.bufferedReader().use { it.readText() }
-        Log.d(TAG,"onCreate: $jsonText")
+        Log.d(TAG, "onCreate: $jsonText")
         val gson = Gson()
-        val oType = object : TypeToken<List<Question>>(){} .type
-        questions = gson.fromJson<List<Question>>(jsonText,oType)
+        val oType = object : TypeToken<List<Question>>() {}.type
+        questions = gson.fromJson<List<Question>>(jsonText, oType)
+        Log.d(TAG, "onCreate: $questions")
+        // construct quiz object
+        quiz = Quiz(questions)
+    }
 
-        }
-        }
-
-
-
-
+}
